107aaf0bf8506f1f906d33604645dd8df881b749
zane.wang <parazane@gmail.com>
2022-11-08 22:36:07 +1100

    init

---
+++ questions/leetcode/editor/en/Q1TwoSum.java
@@ -0,0 +1,125 @@
+package questions.leetcode.editor.en;
+
+//Given an array of integers nums and an integer target, return indices of the 
+//two numbers such that they add up to target. 
+//
+// You may assume that each input would have exactly one solution, and you may 
+//not use the same element twice. 
+//
+// You can return the answer in any order. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [3,2,4], target = 6
+//Output: [1,2]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: nums = [3,3], target = 6
+//Output: [0,1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 2 <= nums.length <= 10⁴ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// -10⁹ <= target <= 10⁹ 
+// Only one valid answer exists. 
+// 
+//
+// 
+//Follow-up: Can you come up with an algorithm that is less than 
+//O(n²) time complexity?
+//
+// Related Topics Array Hash Table 👍 39879 👎 1281
+
+
+//Given an array of integers nums and an integer target, return indices of the 
+//two numbers such that they add up to target. 
+//
+// You may assume that each input would have exactly one solution, and you may 
+//not use the same element twice. 
+//
+// You can return the answer in any order. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [3,2,4], target = 6
+//Output: [1,2]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: nums = [3,3], target = 6
+//Output: [0,1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 2 <= nums.length <= 10⁴ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// -10⁹ <= target <= 10⁹ 
+// Only one valid answer exists. 
+// 
+//
+// 
+//Follow-up: Can you come up with an algorithm that is less than 
+//O(n²) time complexity?
+//
+// Related Topics Array Hash Table 👍 39879 👎 1281
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q1TwoSum {
+    public static void main(String[] args) {
+        Solution solution = new Q1TwoSum().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer, Integer> map = new HashMap<>();
+
+            for (int i = 0; i < nums.length; i++) {
+                int n = target - nums[i];
+                if (map.containsKey(n)) {
+                    return new int[]{i, map.get(n)};
+                }
+                map.put(nums[i], i);
+            }
+
+            return new int[]{};
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ questions/leetcode/editor/en/Q2AddTwoNumbers.java
@@ -0,0 +1,126 @@
+package questions.leetcode.editor.en;
+
+//You are given two non-empty linked lists representing two non-negative 
+//integers. The digits are stored in reverse order, and each of their nodes contains a 
+//single digit. Add the two numbers and return the sum as a linked list. 
+//
+// You may assume the two numbers do not contain any leading zero, except the 
+//number 0 itself. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: l1 = [2,4,3], l2 = [5,6,4]
+//Output: [7,0,8]
+//Explanation: 342 + 465 = 807.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: l1 = [0], l2 = [0]
+//Output: [0]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+//Output: [8,9,9,9,0,0,0,1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in each linked list is in the range [1, 100]. 
+// 0 <= Node.val <= 9 
+// It is guaranteed that the list represents a number that does not have 
+//leading zeros. 
+// 
+//
+// Related Topics Linked List Math Recursion 👍 22621 👎 4381
+
+
+//You are given two non-empty linked lists representing two non-negative 
+//integers. The digits are stored in reverse order, and each of their nodes contains a 
+//single digit. Add the two numbers and return the sum as a linked list. 
+//
+// You may assume the two numbers do not contain any leading zero, except the 
+//number 0 itself. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: l1 = [2,4,3], l2 = [5,6,4]
+//Output: [7,0,8]
+//Explanation: 342 + 465 = 807.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: l1 = [0], l2 = [0]
+//Output: [0]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+//Output: [8,9,9,9,0,0,0,1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in each linked list is in the range [1, 100]. 
+// 0 <= Node.val <= 9 
+// It is guaranteed that the list represents a number that does not have 
+//leading zeros. 
+// 
+//
+// Related Topics Linked List Math Recursion 👍 22621 👎 4381
+
+
+public class Q2AddTwoNumbers {
+    public static void main(String[] args) {
+        Solution solution = new Q2AddTwoNumbers().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            return inner(l1, l2, 0);
+        }
+
+        ListNode inner(ListNode l1, ListNode l2, int carry) {
+            if (l1 == null && l2 == null && carry == 0) return null;
+            ListNode res = new ListNode();
+            int v1 = l1 == null ? 0 : l1.val;
+            int v2 = l2 == null ? 0 : l2.val;
+
+            res.val = (v1 + v2 + carry) % 10;
+            int newCarry = (v1 + v2 + carry) / 10;
+
+            res.next = inner(l1 == null ? null : l1.next, l2 == null ? null : l2.next, newCarry);
+
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
