18abf924fe2721a6df1925bab0e304b30ca1350a
zane.wang <parazane@gmail.com>
2023-01-31 20:59:03 +1100

    1/31

--- questions/leetcode/editor/en/Q300LongestIncreasingSubsequence.java
+++ questions/leetcode/editor/en/Q300LongestIncreasingSubsequence.java
@@ -90,19 +90,45 @@ import java.util.Deque;
 public class Q300LongestIncreasingSubsequence {
     public static void main(String[] args) {
         Solution solution = new Q300LongestIncreasingSubsequence().new Solution();
+        System.out.println(solution.lengthOfLIS(new int[]{10, 9, 2, 5, 3, 4}));
+
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int lengthOfLIS(int[] nums) {
             if (nums.length == 0) return 0;
-            Deque<Integer> s = new ArrayDeque<>();
-            int res = 1;
-            for (int n : nums) {
-                if (s.isEmpty()) {
-                    s.push(n);
+
+            int[] tails = new int[nums.length];
+
+            int t = 0;
+            tails[0] = nums[0];
+
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] > tails[t]) {
+                    t++;
+                    tails[t] = nums[i];
+                } else {
+                    int index = bi(tails, 0, t, nums[i]);
+                    tails[index] = nums[i];
                 }
             }
+
+            return t + 1;
+        }
+
+        int bi(int[] nums, int ll, int rr, int tar) { // find smallest larger one than tar
+            int l = ll, r = rr;
+            while (l < r) { // end cond: l + 1 == r OR l = r - 1
+                int mid = (r - l) / 2 + l; // mid-left
+
+                if (nums[mid] == tar) return mid;
+
+                if (nums[mid] < tar) l = mid + 1;
+                else r = mid;
+            }
+
+            return l;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ questions/leetcode/editor/en/Q322CoinChange.java
@@ -0,0 +1,125 @@
+package questions.leetcode.editor.en;
+
+//You are given an integer array coins representing coins of different 
+//denominations and an integer amount representing a total amount of money. 
+//
+// Return the fewest number of coins that you need to make up that amount. If 
+//that amount of money cannot be made up by any combination of the coins, return -1.
+// 
+//
+// You may assume that you have an infinite number of each kind of coin. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: coins = [1,2,5], amount = 11
+//Output: 3
+//Explanation: 11 = 5 + 5 + 1
+// 
+//
+// Example 2: 
+//
+// 
+//Input: coins = [2], amount = 3
+//Output: -1
+// 
+//
+// Example 3: 
+//
+// 
+//Input: coins = [1], amount = 0
+//Output: 0
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= coins.length <= 12 
+// 1 <= coins[i] <= 2³¹ - 1 
+// 0 <= amount <= 10⁴ 
+// 
+//
+// Related Topics Array Dynamic Programming Breadth-First Search 👍 15159 👎 353
+//
+
+
+//You are given an integer array coins representing coins of different 
+//denominations and an integer amount representing a total amount of money. 
+//
+// Return the fewest number of coins that you need to make up that amount. If 
+//that amount of money cannot be made up by any combination of the coins, return -1.
+// 
+//
+// You may assume that you have an infinite number of each kind of coin. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: coins = [1,2,5], amount = 11
+//Output: 3
+//Explanation: 11 = 5 + 5 + 1
+// 
+//
+// Example 2: 
+//
+// 
+//Input: coins = [2], amount = 3
+//Output: -1
+// 
+//
+// Example 3: 
+//
+// 
+//Input: coins = [1], amount = 0
+//Output: 0
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= coins.length <= 12 
+// 1 <= coins[i] <= 2³¹ - 1 
+// 0 <= amount <= 10⁴ 
+// 
+//
+// Related Topics Array Dynamic Programming Breadth-First Search 👍 15159 👎 353
+//
+
+
+import java.util.Arrays;
+
+public class Q322CoinChange {
+    public static void main(String[] args) {
+        Solution solution = new Q322CoinChange().new Solution();
+        System.out.println(solution.coinChange(new int[]{1, 2, 5}, 11));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int coinChange(int[] coins, int amount) {
+            if (amount == 0) return 0;
+            int[] dp = new int[amount + 1];
+            Arrays.fill(dp, amount + 1);
+            Arrays.sort(coins);
+
+            for (int i = 1; i < amount + 1; i++) {
+                for (int j = 0; j < coins.length && coins[j] <= i; j++) {
+                    if (coins[j] == i) {
+                        dp[i] = 1;
+                        break;
+                    } else {
+                        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                    }
+                }
+            }
+
+            return dp[amount] >= amount + 1 ? -1 : dp[amount];
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
